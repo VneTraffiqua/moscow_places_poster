a9a8bc4d943f160b5c3e3101a7186446c990e27f
VneTraffiqua <safe72again@gmail.com>
2023-01-24 04:58:17 -0800

    Add frontend

--- places_poster/urls.py
+++ places_poster/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from places_poster import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_index)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- places_poster/views.py
+++ places_poster/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 
 def show_index(request):
-    return HttpResponse('Здесь будет карта')
+    return render(request, 'index.html')
