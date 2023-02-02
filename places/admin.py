6bc79890256d33cb4379579960b30b150ce0f21e
VneTraffiqua <safe72again@gmail.com>
2023-02-02 01:17:22 -0800

    Added photos in lines to Place model in adminpanel

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Place, Photo
 
-admin.site.register(Place)
+
+class PhotosInline(admin.TabularInline):
+    model = Photo
+
+
 admin.site.register(Photo)
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'description_short'
+    ]
+    inlines = [PhotosInline]
