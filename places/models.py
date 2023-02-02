682aa125a9df30693ea3281683e1c1a6d9b4e8f8
VneTraffiqua <safe72again@gmail.com>
2023-02-02 03:34:24 -0800

    Added HTMLField in place model

--- places/models.py
+++ places/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
@@ -12,7 +13,7 @@ class Place(models.Model):
         max_length=255,
         help_text='Введите короткое описание'
     )
-    description_long = models.TextField(
+    description_long = HTMLField(
         verbose_name='Описание',
         help_text='Введите описание'
     )
--- places_poster/urls.py
+++ places_poster/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from places_poster import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,5 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_index),
-    path('places/<int:place_id>', views.show_place_info, name='place_info')
+    path('places/<int:place_id>', views.show_place_info, name='place_info'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
