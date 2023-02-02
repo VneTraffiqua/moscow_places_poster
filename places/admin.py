efe300c369dd296295d1bcf0a7f57e548d07ada8
VneTraffiqua <safe72again@gmail.com>
2023-02-02 01:53:03 -0800

    Added images preview in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,16 +1,28 @@
 from django.contrib import admin
 from .models import Place, Photo
+from django.utils.html import format_html
 
 
 class PhotosInline(admin.TabularInline):
     model = Photo
+    readonly_fields = ['show_image', ]
+
+    def show_image(self, obj):
+        return format_html(
+            f'<img src="{obj.image.url}" height=200 />'
+        )
 
 
 admin.site.register(Photo)
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = [
         'title', 'description_short'
     ]
+
     inlines = [PhotosInline]
+
+
+
